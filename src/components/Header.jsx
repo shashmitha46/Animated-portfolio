119c512a973c24b2584e8ee4150192fd0f68597f
shashmitha46 <itzzshashmitha@gmail.com>
2025-10-04 13:15:46 +0530

    added mobile menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
 import { motion } from "framer-motion"
 import { a } from "framer-motion/client"
-import { FiGithub, FiLinkedin } from "react-icons/fi"
+import { FiGithub, FiLinkedin, FiMenu, FiX } from "react-icons/fi"
 import { SiLeetcode } from "react-icons/si"
+import { useState } from "react"
 
 const Header = () => {
+    // Toggle the menu open/close
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleMenu = () => setIsOpen(!isOpen);
+
     return (
         <header className="absolute w-full z-50 transition-all duration-300">
             <div className="container mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between h-16 md:h-20">
@@ -46,13 +51,15 @@ const Header = () => {
                             className="relative text-gray-800 dark:text-gray-200 font-medium hover:text-gray-200 hover:violet-600 dark:hover:text-violet-400 font-medium transition-colors duration-300 group"
                             href="#">
                             {item}
-                            <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-violet-600 dark:bg-violet-400 transition-all duration-300 group-hover:w-full"></span>
+                            <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-violet-600 dark:bg-violet-400  group-hover:w-full transition-all duration-300"></span>
                         </motion.a>
                     ))}
 
                 </nav>
+
+
                 {/* social icons - desktop */}
-                <div className="md:flex hidden itrms-center space-x-4">
+                <div className="md:flex hidden items-center space-x-4">
                     <motion.a
                         initial={{ opacity: 0, scale: 0.5 }}
                         animate={{ opacity: 1, scale: 1 }}
@@ -75,6 +82,7 @@ const Header = () => {
 
                         className="text-gray-700 dark:text-gray-300 hover:text-violet-600 dark:hover:text-violet-400 transition-colors duration-300" href="#">
                         <FiLinkedin className="w-5 h-5" />
+                        <a href="#"></a>
                     </motion.a>
 
                     <motion.a
@@ -88,25 +96,51 @@ const Header = () => {
                         className="text-gray-700 dark:text-gray-300 hover:text-violet-600 dark:hover:text-violet-400 transition-colors duration-300" href="#">
                         <SiLeetcode className="w-5 h-5" />
                     </motion.a>
+
+
+                    {/* Hire me Button */}
+                    <motion.button
+                        initial={{ opacity: 0, scale: 0.8 }}
+                        animate={{ opacity: 1, scale: 1 }}
+                        transition={{
+                            delay: 1.6,
+                            duration: 0.8,
+                            type: "spring",
+                            stiffness: 100,
+                            damping: 15,
+                        }}
+                        className="m1-4 px-4 py-2 rounded-xl bg-gradient-to-r from-gray-400 to-gray-100 text-violet-700 font-bold hover:from-violet-700 hover:to-purple-700 hover:text-white transition-all duration-500">
+                        Hire Me
+                    </motion.button>
                 </div>
 
-                {/* Hire me Button */}
-                <motion.button
-                initial={{ opacity: 0, scale: 0.8}} 
-                animate={{ opacity: 1, scale: 1}}
-                transition={{
-                    delay: 1.6,
-                    duration: 0.8,
-                    type: "spring",
-                    stiffness: 100,
-                    damping: 15,
-                }}
-                className="m1-4 px-4 py-2 rounded-xl bg-gradient-to-r from-gray-400 to-gray-100 text-violet-700 font-bold hover:from-violet-700 hover:to-purple-700 hover:text-white transition-all duration-500">
-                    Hire Me
-                </motion.button>
 
+
+                {/* Mobile Menu */}
+                <div className="md:hidden flex items-center">
+                    <motion.button
+                        whileTap={{ scale: 0.7 }}
+                        onClick={toggleMenu}
+                        className="text-gray-300">
+                        { isOpen ? <FiX className="h-6 w-6" /> : <FiMenu className="h-6 w-6" />}
+
+                    </motion.button>
+                </div>
             </div>
 
+            {/* Mobile Menu Items */}
+            <div className="md:hidden overflow-hidden bg-white dark:bg-gray-900 shadow-lg px-4 py-5 space-y-5">
+                <nav className="flex flex-col space-y-3">
+                    {['Home', 'About', 'Projects', 'Contact'].map((item) => (
+                        <a onClick={toggleMenu} className="text-gray-300 font-medium py-2" key = {item} href="#">
+                            {item}
+
+                        </a>
+                    ))}
+
+                </nav>
+
+            </div>
         </header>
     )
 }
